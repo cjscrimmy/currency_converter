60b1697c32544118920e2388a6fbbe4023a9eb11
cjscrimmy <craigscrimgeour@yahoo.com>
2020-05-12 20:03:15 +0100

    added div to display results of the conversion from euros. couldnt get it to work

--- src/app.js
+++ src/app.js
@@ -7,12 +7,21 @@ document.addEventListener("DOMContentLoaded", () => {
             currencies: {},
             selectedCurrency: null
         },
+        computed:{
+            conversionFromEurosResult: function (){
+                return this.convertFromEuros;
+            }
+        },
         methods:{
             fetchCurrencies: function(){
                 fetch("https://api.exchangeratesapi.io/latest")
                     .then(resourses => resourses.json())
                     .then(currencies => this.currencies = currencies.rates)
+            },
+            convertFromEuros: function(){
+               return this.euroValue * this.selectedCurrency;
             }
+
         },
         mounted(){
             this.fetchCurrencies()
